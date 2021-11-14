76f5216022aa628917129117fc41dfdad6a499ea
daniel <47399125+zhenglu8@users.noreply.github.com>
2021-11-13 22:20:35 -0800

    added synonyms to NLTK.py

--- NLTK.py
+++ NLTK.py
@@ -1,10 +1,11 @@
+from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 import nltk
+from nltk.corpus import wordnet
 nltk.download('punkt')
 nltk.download('wordnet')
 
-from nltk.stem import WordNetLemmatizer
 
 ps = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
@@ -12,12 +13,18 @@ lemmatizer = WordNetLemmatizer()
 example_text = "award awarding accolade honor honour laurels prize present grant"
 words = word_tokenize(example_text)
 
+# Stemming
 for i in words:
     print(ps.stem(i))
     if ps.stem(i) == "spam":
         print("ACTION")
-'''
+# Lemmatization
 for i in words:
-   print(lemmatizer.lemmatize(i))
-'''
+    print(lemmatizer.lemmatize(i))
 
+# Synonyms
+synonyms = []
+for syn in wordnet.synsets('prize'):
+    for lemma in syn.lemmas():
+        synonyms.append(lemma.name())
+print(synonyms)
